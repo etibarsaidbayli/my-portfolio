f4139e529c6104e995d7267ef9a12e2317d647b7
Etibar Saidbayli <etibarseidbekov@gmail.com>
2022-11-11 21:59:52 +0300

    november CV

--- src/pages/home/sections/HeroSection.js
+++ src/pages/home/sections/HeroSection.js
@@ -36,8 +36,7 @@ function HeroSection() {
               </AnchorTheme>
               <AnchorTheme
                 themeColor={themeColor}
-                href="https://drive.google.com/file/d/1AWmo67cXf7UvuoflRbsuOt2PvmeW-2Sn/view"
-                rel="noreferrer"
+                href="https://drive.google.com/file/d/1EVrD5L7yvAsTMJHQaoCryPP3GwxJmgRi/view"
                 download
                 className="btn px-5 py-3"
               >
